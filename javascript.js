2338e64707092d675ced6d43f794fe338c95fa24
Jackson Jewell <jackson.t.jewell@gmail.com>
2019-12-07 15:25:41 -0600

    javascript back to tuesday

--- javascript.js
+++ javascript.js
@@ -1,23 +1,28 @@
 var searchItem;
 var queryURL;
 
-///$("someSubmitButton").onclick(startSearch);
+$("#btn-submit").on("click",startSearch);
 
 
 
 function startSearch(){
+    event.preventDefault();
+    searchItem = $("#searchLocations").val();
+    console.log(searchItem);
     buildQuery("NPS");
     buildQuery("weather");
 }
 
 function callAPI(type){
+    console.log(type);
+    console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
         console.log(response);
         switch(type){
-            case 1:
+            case "1":
                 let nationalPark = response;
                 let parkName = nationalPark.fullName;
                 let parkLoc = nationalPark.directionsURL;
@@ -28,7 +33,7 @@ function callAPI(type){
                 $("#description").text(parkDesc);
                 $("#url").text(parkURL);
                 break;
-            case 2:
+            case "2":
                 let weather = response;
                 break;
             default:
@@ -39,29 +44,29 @@ function callAPI(type){
 
 function buildQuery(searchType){
     switch(searchType){
-        case NPS:
+        case "NPS":
             apiKey="api_key=auId6pdJdBjNrQajHOe6lmOqvegjIh77fAeCZ694";
             queryURL = "https://developer.nps.gov/api/v1/parks?";
             var state = "stateCode="
             var stateCode;
-            var userQuestion;
 
             if(stateCode !== undefined){
-                var queryURL = queryURL+state+stateCode+"&"+apiKey;
-            }else if(userQuestion !== undefined){
-                var queryURL = queryURL+"q="+userQuestion+"&"+apiKey;
+                queryURL = queryURL+state+stateCode+"&"+apiKey;
+            }else if(searchItem !== undefined){
+                queryURL = queryURL+"q="+searchItem+"&"+apiKey;
             }else{
-                var queryURL = queryURL+"&"+apiKey;
+                queryURL = queryURL+"&"+apiKey;
             }
-            return queryURL;
+            console.log(queryURL);
+            callAPI("1");
 
             break;
         case "weather":
             queryURL = "https://api.openweathermap.org/data/2.5/weather?q="
-            +userQuestion
+            +searchItem
             +"&APPID=6bd5f328c4eb31862977239b636ff37a";
 
-            callAPI(2);
+            callAPI("2");
 
             break;
         default:
